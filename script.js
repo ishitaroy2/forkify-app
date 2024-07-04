b6d5eadf66ebf1ce178271ae2d708bf34f8157c1
ishitaroy2 <123700704+ishitaroy2@users.noreply.github.com>
2024-07-04 16:22:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,136 +1,179 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-// List of words for game
-const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving'
-];
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
-// Init word
-let randomWord;
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
 
-// Init score
-let score = 0;
+// Keep track of song
+let songIndex = 2;
 
-// Init time
-let time = 10;
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
 
-// Set difficulty to value in ls or medium
-let difficulty =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
-
-// Set difficulty select value
-difficultySelect.value =
-  localStorage.getItem('difficulty') !== null
-    ? localStorage.getItem('difficulty')
-    : 'medium';
-
-// Focus on text on start
-text.focus();
-
-// Start counting down
-const timeInterval = setInterval(updateTime, 1000);
-
-// Generate random word from array
-function getRandomWord() {
-  return words[Math.floor(Math.random() * words.length)];
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
-// Add word to DOM
-function addWordToDOM() {
-  randomWord = getRandomWord();
-  word.innerHTML = randomWord;
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
 }
 
-// Update score
-function updateScore() {
-  score++;
-  scoreEl.innerHTML = score;
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
 }
 
-// Update time
-function updateTime() {
-  time--;
-  timeEl.innerHTML = time + 's';
+// Previous song
+function prevSong() {
+  songIndex--;
 
-  if (time === 0) {
-    clearInterval(timeInterval);
-    // end game
-    gameOver();
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
   }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
-// Game over, show end screen
-function gameOver() {
-  endgameEl.innerHTML = `
-    <h1>Time ran out</h1>
-    <p>Your final score is ${score}</p>
-    <button onclick="location.reload()">Reload</button>
-  `;
+// Next song
+function nextSong() {
+  songIndex++;
 
-  endgameEl.style.display = 'flex';
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
-addWordToDOM();
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
 
 // Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
 
-// Typing
-text.addEventListener('input', e => {
-  const insertedText = e.target.value;
-
-  if (insertedText === randomWord) {
-    addWordToDOM();
-    updateScore();
-
-    // Clear
-    e.target.value = '';
-
-    if (difficulty === 'hard') {
-      time += 2;
-    } else if (difficulty === 'medium') {
-      time += 3;
-    } else {
-      time += 5;
-    }
-
-    updateTime();
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
   }
 });
 
-// Settings btn click
-settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
-// Settings select
-settingsForm.addEventListener('change', e => {
-  difficulty = e.target.value;
-  localStorage.setItem('difficulty', difficulty);
-});
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
+
